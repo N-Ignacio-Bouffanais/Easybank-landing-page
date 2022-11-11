8036d6c7339ee2be8da46037b8a8515561038d3f
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-11-10 23:13:25 -0300

    changes to img width and height and it has become more responsiv :)

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const x = document.getElementById("close")
 const y = document.getElementById("hamburguer")
+const z = document.getElementById("nav-list")
 
 function Open() {
-    if (window.innerWidth <= 830) {
+    if (window.innerWidth <= 900) {
         x.style.display = "block";
         y.style.display = "none";
+        z.style.display = "flex";
     }
     else {
         x.style.display = "none";
@@ -12,9 +14,10 @@ function Open() {
     }
 }
 function Close() {
-    if (window.innerWidth <= 830) {
+    if (window.innerWidth <= 900) {
         x.style.display = "none";
         y.style.display = "block";
+        z.style.display = "none";
     }
     else {
         x.style.display = "none";
